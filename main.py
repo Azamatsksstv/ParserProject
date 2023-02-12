882e6bac713a9a9769023a01e49ddbdf2d7aee73
Azamat <azambylbaev@gmail.com>
2023-02-12 16:02:55 +0500

    'temp'

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
 import queries
-
-url = 'https://www.nur.kz/society/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
-films = soup.findAll('article', class_='block-infinite__item-content')
-data = []
-
-for film in films:
-    link = film.find('a').get('href')
-    title = film.find('h3').text.strip()
-    datetime = film.find('time').text.strip()
-    data.append([link, title, datetime])
+import datetime
+import dateparser
 
 
-queries.drop_table_resource()
-queries.create_table_resource()
-queries.insert_to_resource(data)
+def parse():
+    RESOURCE_NAME = 'NUR.KZ'
+    RESOURCE_URL  = 'https://www.nur.kz/society/'
+    top_tag = 'a'
+    bottom_tag = 'div' #otirik
+    title_cut = 'h3'
+    date_cut = 'time'
+
+    response = requests.get(RESOURCE_URL)
+    soup = BeautifulSoup(response.text, "lxml")
+    news = soup.findAll('article', class_='block-infinite__item-content')
+
+    resource_data = [[RESOURCE_NAME, RESOURCE_URL, top_tag, bottom_tag, title_cut, date_cut]]
+    queries.insert_to_resource(resource_data)
+
+    data = []
+    res_id = str(queries.get_resource_id(RESOURCE_NAME))
+    for news_item in news:
+        link = news_item.find(top_tag).get('href')                           # ссылку на новость
+        title = news_item.find(title_cut).text.strip()                        # заголовок новости.
+        datetime_from_website = news_item.find(date_cut).text.strip()
+        timestamp = dateparser.parse(datetime_from_website)
+        nd_date = datetime.datetime.timestamp(timestamp) * 1000     # дату и время новости в формате Unix time
+        current_time = datetime.datetime.now()
+        s_date = datetime.datetime.timestamp(current_time) * 1000   # дату и время попадания новости в саму таблицу items в формате Unix time
+        not_date = timestamp.strftime("%Y-%m-%d")                   # дату новости в формате Год-Месяц-День
+        data.append([res_id, link, title, nd_date, s_date, not_date])
+
+    queries.insert_to_items(data)
+
+
+def main():
+    queries.drop_table_items()
+    queries.create_table_items()
+    queries.drop_table_resource()
+    queries.create_table_resource()
+
+    parse()
+
+
+if __name__ == '__main__':
+    main()
--- queries.py
+++ queries.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def drop_table_resource():
+def drop_table_items():
     try:
         connection = sqlite3.connect('db.db')
         connection.execute('''drop table items;''')
@@ -9,24 +9,28 @@ def drop_table_resource():
         print(ex)
 
 
-def create_table_resource():
+def create_table_items():
     try:
         connection = sqlite3.connect('db.db')
         connection.execute('''create table items
                             (id integer primary key autoincrement,
-                             link text not null, 
-                             title text not null, 
-                             datetime date not null);''')
+                            res_id int,
+                            link text not null, 
+                            title text not null, 
+                            content text,
+                            nd_date int(11),
+                            s_date int(11),
+                            not_date date not null);''')
         connection.close()
     except Exception as ex:
         print(ex)
 
 
-def insert_to_resource(data):
+def insert_to_items(data):
     try:
         connection = sqlite3.connect('db.db')
         cursor = connection.cursor()
-        query = '''INSERT INTO items (link, title, datetime) VALUES (?, ?, ?)'''
+        query = '''INSERT INTO items (res_id, link, title, nd_date, s_date, not_date) VALUES (?, ?, ?, ?, ?, ?)'''
         values = data
         cursor.executemany(query, values)
         connection.commit()
@@ -35,3 +39,56 @@ def insert_to_resource(data):
     except Exception as ex:
         print(ex)
 
+
+def drop_table_resource():
+    try:
+        connection = sqlite3.connect('db.db')
+        connection.execute('''drop table resource;''')
+    except Exception as ex:
+        print(ex)
+
+
+def create_table_resource():
+    try:
+        connection = sqlite3.connect('db.db')
+        connection.execute('''create table resource
+                            (RESOURCE_ID  integer primary key autoincrement,
+                            RESOURCE_NAME  varchar(255),
+                            RESOURCE_URL  varchar(255), 
+                            top_tag  varchar(255),
+                            bottom_tag  varchar(255),
+                            title_cut  varchar(255),
+                            date_cut  varchar(255));''')
+        connection.close()
+        print("success")
+    except Exception as ex:
+        print(ex)
+
+
+def insert_to_resource(datas):
+    try:
+        connection = sqlite3.connect('db.db')
+        cursor = connection.cursor()
+        query = '''INSERT INTO resource (RESOURCE_NAME, RESOURCE_URL, top_tag, bottom_tag, title_cut, date_cut) VALUES (?, ?, ?, ?, ?, ?)'''
+        values = datas
+        cursor.executemany(query, values)
+        connection.commit()
+        connection.close()
+        print("inserted to resource")
+    except Exception as ex:
+        print(ex)
+
+
+def get_resource_id(res_name):
+    try:
+        connection = sqlite3.connect('db.db')
+        cursor = connection.execute('''select RESOURCE_ID from resource where RESOURCE_NAME = ?''', (res_name,))
+        results = cursor.fetchall()
+        if results:
+            resource_id = results[0][0]
+        else:
+            resource_id = None
+        connection.close()
+        return resource_id
+    except Exception as ex:
+        print(ex)
